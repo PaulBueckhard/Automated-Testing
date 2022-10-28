ba11c2d3206004d02d9fbcccd9c4016f8ecb1d4a
PaulBueckhard <paul.bueckhard@code.berlin>
2022-10-28 12:58:10 +0200

    Added spacy person test

--- flask-ner/test/test_ner_client.py
+++ flask-ner/test/test_ner_client.py
@@ -19,3 +19,12 @@ class TestNerClient(unittest.TestCase):
         ner = NamedEntityClient(model)
         ents = ner.get_ents("Berlin is the capital of Germany")
         self.assertIsInstance(ents, dict)
+
+    def test_get_ents_given_spacy_person_is_returned(self):
+        model = NerModelTestDouble("eng")
+        doc_ents = [{"text": "Nikola Tesla", "label_": "PERSON"}]
+        model.returns_doc_ents(doc_ents)
+        ner = NamedEntityClient(model) 
+        result = ner.get_ents("...")
+        expected_result = { "ents": [{"ent": "Nikola Tesla", "label": "Person"}], "html": "" }
+        self.assertListEqual(result["ents"], expected_result["ents"])
