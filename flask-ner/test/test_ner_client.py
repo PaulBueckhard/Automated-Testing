f0381879ac65379befbb66c263b9334dec487520
PaulBueckhard <paul.bueckhard@code.berlin>
2022-10-28 12:43:55 +0200

    Added test doubles

--- flask-ner/test/test_ner_client.py
+++ flask-ner/test/test_ner_client.py
@@ -1,5 +1,6 @@
 import unittest
 from ner_client import NamedEntityClient
+from test_doubles import NerModelTestDouble
 
 class TestNerClient(unittest.TestCase):
 
@@ -7,11 +8,13 @@ class TestNerClient(unittest.TestCase):
     #   html: "<span>..."}
     
     def test_get_ents_returns_dictionary_given_empty_string(self):
-        ner = NamedEntityClient()
+        model = NerModelTestDouble("eng")
+        model.returns_doc_ents([])
+        ner = NamedEntityClient(model)
         ents = ner.get_ents("")
         self.assertIsInstance(ents, dict)
     
     def test_get_ents_returns_list_given_nonempty_string(self):
-        ner = NamedEntityClient()
+        ner = NamedEntityClient(model)
         ents = ner.get_ents("Berlin is the capital of Germany")
         self.assertIsInstance(ents, dict)
