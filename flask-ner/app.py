9d872fcadd6b9780c55b0071eee4d135c92dd3f4
PaulBueckhard <paul.bueckhard@code.berlin>
2022-11-08 22:49:30 +0100

    updated html and css

--- flask-ner/app.py
+++ flask-ner/app.py
@@ -1,23 +1,24 @@
 from flask import Flask, render_template, request
 import json
 import spacy
+from spacy import displacy
 from ner_client import NamedEntityClient
 
 app = Flask(__name__)
 
 ner = spacy.load("en_core_web_sm")
-ner = NamedEntityClient(ner)
+ner = NamedEntityClient(ner, displacy)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/ner", methods=["POST"])
+@app.route('/ner', methods=['POST'])
 def get_named_ents():
     data = request.get_json()
-    result = ner.get_ents(data["sentence"])
-    response = { "entities": result.get("ents"), "html": result.get("html") }
+    result = ner.get_ents(data['sentence'])
+    response = { "entities": result.get('ents'), "html": result.get('html') }
     return json.dumps(response)
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
--- flask-ner/ner_client.py
+++ flask-ner/ner_client.py
@@ -1,20 +1,22 @@
 class NamedEntityClient:
-    def __init__(self, model):
+    def __init__(self, model, displacy):
         self.model = model
+        self.displacy = displacy
 
     def get_ents(self, sentence):
         doc = self.model(sentence)
-        entities = [{ "ent": ent.text, "label": self.map_label(ent.label_)} for ent in doc.ents]
-        return { "ents": entities, "html": ""}
+        html = self.displacy.render(doc, style="ent")
+        entities = [{ 'ent': ent.text, 'label': self.map_label(ent.label_) } for ent in doc.ents]
+        return { 'ents': entities, 'html': html }
 
     @staticmethod
     def map_label(label):
         label_map = {
-            "PERSON"    : "Person",
-            "NORP"      : "Group",
-            "LOC"       : "Location",
-            "GPE"       : "Location",
-            "LANGUAGE"  : "Language"
+            'PERSON'  : 'Person',
+            'NORP'    : 'Group',
+            'LOC'     : 'Location',
+            'GPE'     : 'Location',
+            'LANGUAGE': 'Language'
         }
 
         return label_map.get(label)
